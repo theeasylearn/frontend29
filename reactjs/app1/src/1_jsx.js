f1060bbcf2046495d1cbab0f4e7c55a49dd5b640
theeasylearn <theeasylearn@gmail.com>
2024-06-24 19:24:42 +0530

    day 2

---
+++ reactjs/app1/src/1_jsx.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<div><h1>Hello world...</h1></div>);
--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let output = <>
+    <h1>जन गण मन</h1>
+    <hr/>
+    <p>
+        जन गण मन अधिनायक जय हे<br />
+        भारत भाग्य विधाता<br />
+        पंजाब सिंधु गुजरात मराठा<br />
+        द्राविड़ उत्कल बंग<br />
+        विन्ध्य हिमाचल यमुना गंगा<br />
+        उच्छल जलधि तरंग<br />
+        तव शुभ नामे जागे<br />
+        तव शुभ आशीष मांगे<br />
+        गाहे तव जय गाथा<br />
+        जन गण मंगलदायक जय हे<br />
+        भारत भाग्य विधाता<br />
+        जय हे, जय हे, जय हे<br />
+        जय जय जय जय हे
+    </p>
+</>
+root.render(output);
