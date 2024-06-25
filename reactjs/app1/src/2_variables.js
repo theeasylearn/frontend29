d94b32b5a0d0ff3ab2b2c67893485ec2f6146bc8
theeasylearn <theeasylearn@gmail.com>
2024-06-25 19:13:35 +0530

    test

---
+++ reactjs/app1/src/2_variables.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+let output = <>
+    <h1>जन गण मन</h1>
+    <hr/>
+    <p>
+        जन गण मन अधिनायक जय हे<br />
+        भारत भाग्य विधाता<br />
+        पंजाब सिंधु गुजरात मराठा<br />
+        द्राविड़ उत्कल बंग<br />
+        विन्ध्य हिमाचल यमुना गंगा<br />
+        उच्छल जलधि तरंग<br />
+        तव शुभ नामे जागे<br />
+        तव शुभ आशीष मांगे<br />
+        गाहे तव जय गाथा<br />
+        जन गण मंगलदायक जय हे<br />
+        भारत भाग्य विधाता<br />
+        जय हे, जय हे, जय हे<br />
+        जय जय जय जय हे
+    </p>
+</>
+root.render(output);
--- reactjs/app1/src/3_expression.js
+++ reactjs/app1/src/3_expression.js
@@ -5,14 +5,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 var num1 = 30;
 var num2 = 4;
 var result = (<>
-    <h1 className='myHeading'>Expression in reactjs</h1> <hr/>
-    <ul className='myList'>
+    <h1>Expression in reactjs</h1> <hr/>
+    <ul>
         <li>num1 = {num1}</li>
         <li>num2 = {num2}</li>
         <li>Addition = {num1 + num2}</li>
         <li>Subtraction = {num1 - num2}</li>
         <li>Multiplication = {num1 * num2}</li>
         <li>Division = {num1/num2}</li>
-    </ul>
-</>)
+    </ul>)
+</>
 root.render(result);
