f9169921409513f17dc63f8705b780702de4c762
theeasylearn <theeasylearn@gmail.com>
2024-09-02 17:54:16 +0530

    login api called

--- reactjs/shop_admin/src/Login.js
+++ reactjs/shop_admin/src/Login.js
@@ -1,17 +1,20 @@
 import { useState } from "react";
 import axios from 'axios';
-//
+import {useNavigate} from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export default function AdminLogin() {
   let [email,setEmail] = useState();
   let [password,setPassword] = useState();
+  let navigate = useNavigate();
   let checkLogin = function(e)
   {
       e.preventDefault();
       console.log(email,password);
-      let apiAddress = "https://theeasylearnacademy.com/shop/ws/admin_login.php?email=admin@gmail.com&password=123123";
+      let apiAddress = "https://theeasylearnacademy.com/shop/ws/admin_login.php";
       let form = new FormData();
-      form.append('email',email);
-      form.append('password',password);
+      form.append('email',email); //admin@gmail.com
+      form.append('password',password); //123123
 
       //api calling
       axios({
@@ -21,12 +24,33 @@ export default function AdminLogin() {
         responseType:'json'
       }).then((response) => {
         console.log(response);
+        let error = response.data[0]['error'];
+        if(error !== 'no')
+          toast(error);
+        else 
+        {
+          let success = response.data[1]['success'];
+          let message = response.data[2]['message'];
+          if(success === 'no')
+            alert(message);
+          else 
+          {
+            alert(message);
+            //display another screen to user (dashboard)
+            navigate("/dashboard");
+
+          }
+        }
+      }).catch((error) => {
+          console.log(error);
+          if(error.code === 'ERR_NETWORK')
+            alert('either server is offline or internet is not available or check api address');
       });
 
   }
   return (
     <div className="container">
-      {/* Outer Row */}
+      <ToastContainer />
       <div className="row justify-content-center">
         <div className="col-xl-10 col-lg-12 col-md-9">
           <div className="card o-hidden border-0 shadow-lg my-5">
