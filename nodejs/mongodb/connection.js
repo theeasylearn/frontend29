7960916251a7c4a30c2bc6099403af6f0cfbed42
theeasylearn <theeasylearn@gmail.com>
2024-12-12 20:11:56 +0530

    insert document

--- nodejs/mongodb/connection.js
+++ nodejs/mongodb/connection.js
@@ -1,12 +1,19 @@
 var mongodb = require('mongodb');
 var client = mongodb.MongoClient;
 var databaseURL  = "mongodb://0.0.0.0:27017/frontend29";
-client.connect(databaseURL,function(error,database){
-    if(error!=null)
-        console.log(error.message,error.code);
-    else 
-    {
-        console.log('database is connected.');
-    }
-
-});
+//promise based function
+var connectToDatabase = new Promise((resolve,reject) => {
+    client.connect(databaseURL,(error,database) => {
+        if(error!=null)
+        {
+            console.log(error.message,error.code);
+            reject(error);
+        }
+        else 
+        {
+            console.log('database is connected.');
+            resolve(database);
+        }
+    });  
+})
+module.exports.database = connectToDatabase;
---
+++ nodejs/mongodb/insert_document.js
@@ -0,0 +1,1 @@
+var database = require('./connection.js');
