df02f012598ef54f22e7483c36e903bca687abf5
theeasylearn <theeasylearn@gmail.com>
2024-12-16 20:20:33 +0530

    update document

--- nodejs/mongodb/update_document.js
+++ nodejs/mongodb/update_document.js
@@ -1,14 +1,14 @@
 var connection = require('./connection.js').database;
 
-var object = {source:'Bhavnagar',destination:'Ahmedabad',distance:180.25};
-
+var updatedObject = {$set:{source:'Bhavnagar',destination:'Baroda',distance:250.90,price:300}};
+var condition = {source:'Bhavnagar'};
 connection.then((db) => {
         var dbInstance = db.db('frontend29');
-        dbInstance.collection('data').insertOne(object, function (error, response) {
+        dbInstance.collection('data').updateOne(condition,updatedObject, function (error, response) {
             if (error != null) {
                 console.log('There is some error in inserting the document:', error.message);
             } else {
-                console.log('Document has been inserted successfully:', response.insertedId);
+                console.log('Document has been updated successfully:', response.insertedId);
             }
             db.close(); // Close the connection after the operation
         });
